54dcb79edb549e689d84ccd2f43f39292bc23793
Philip Cass <philip@digitgaming.com>
2018-02-15 15:43:14 +0000

    first commit

---
+++ Assets/Editor/LogFile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LogFile  {
+
+	static void PerformBuild ()
+	{
+		Debug.Log("Hello From Unity!!");
+	}
+}
